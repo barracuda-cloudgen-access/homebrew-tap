517cb5368fe3cc7bd7fc1a9351acc893e67918d8
Jose Pereira <onaips@gmail.com>
2021-05-31 18:23:14 +0100

    Add script to get package hashes

---
+++ scripts/get_package_hashes.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# Prints the package sha256 hash for all platform files
+# Useful to create new homebrew formulas
+set -e
+set -o pipefail
+
+VERSION=$1
+DOWNLOAD_URL='https://github.com/barracuda-cloudgen-access/access-cli/releases/download/v%VERSION/access-cli_%VERSION_%PLATFORM.tar.gz'
+
+PLATFORMS="macOS_x86_64 Linux_x86_64 Linux_i386"
+
+function print_usage() {
+    echo "Usage: $0 [version]"
+}
+
+if [[ -z "$VERSION" ]]; then
+    print_usage
+    exit 1
+fi
+
+echo Version: "$VERSION"
+
+for PLATFORM in $PLATFORMS; do
+    URL=${DOWNLOAD_URL//\%VERSION/$VERSION}
+    URL=${URL//\%PLATFORM/$PLATFORM}
+
+    echo -e "\nGetting SHA256 for $PLATFORM - $URL"
+    TMPFILE="/tmp/access-cli_${VERSION}_${PLATFORM}.tar.gz"
+    if ! curl -sfL --show-error "$URL" > "$TMPFILE"; then
+        exit 1
+    fi
+    sha256sum "$TMPFILE"
+done
